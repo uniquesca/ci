88e9dc85642474e0dfaa25098a46781606e1c67e
George Shestayev <georges@uniques.ca>
2023-06-30 09:24:58 -0700

    Update: improved default ci environment determination

--- get-default-ci-environment/index.js
+++ get-default-ci-environment/index.js
@@ -5,14 +5,23 @@ import {CiPhpVersion} from "../src/classes/CiPhpVersion.js";
 
 try {
     const env = CiEnvironment.fromEnvironmentFile();
+
     let job;
-    for (let key in env.job_matrix) {
-        if (env.job_matrix[key].default) {
-            job = env.job_matrix[key];
-            break;
+    if (env.job_matrix.length === 1) {
+        // If there is only one job, using it as default
+        job = env.job_matrix[0];
+    } else {
+        // Trying to find job market as default
+        for (let key in env.job_matrix) {
+            if (env.job_matrix[key].default) {
+                job = env.job_matrix[key];
+                break;
+            }
         }
     }
+
     if (!job) {
+        // If no job found, creating default one
         job = new CiJob({
             os: "ubuntu-latest",
             php: new CiPhpVersion({
