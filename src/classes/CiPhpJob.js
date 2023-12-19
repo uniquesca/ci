7ce598f6ac8953457a2d3226106d04b30622260b
George Shestayev <15987211+georgique@users.noreply.github.com>
2023-12-18 17:06:22 -0800

    New: introduced "locked" flag for PHP environments (#3)
    * Update: CiJob renamed to CiPhpJob
    * Update: added "locked" flag to CiPhpJob
    * Update: improved QA checks to choose between composer install and update depending on the CiPhpJob.locked flag
    * Update: added debug output into `prepare-ci-matrix` workflow

--- src/classes/CiPhpJob.js
+++ src/classes/CiPhpJob.js
@@ -1,12 +1,13 @@
 import {CiPhpVersion} from "./CiPhpVersion.js";
 
-export class CiJob {
+export class CiPhpJob {
     os = "ubuntu-latest";
     php = new CiPhpVersion({
         version: "8.1",
         extensions: ""
     });
-    default = false;
+    default = false; // Will be used as a default environment in various CI tasks
+    locked = false; // Affects Composer - it will use install if locked, update otherwise
 
     constructor(input) {
         const copy = {...input};
